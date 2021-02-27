2adc5465373f238d756a324162f9ddb48a5ab8ae
Saad Rahman <itssaadrahman@gmail.com>
2021-02-27 20:09:00 +0600

    another challenge

--- js/challenge-03.js
+++ js/challenge-03.js
@@ -13,12 +13,12 @@ FUNDAMENTALS */
 
 function getGrade (s1, s2, s3) {
     // Code here
-    return  score = (s1 + s2 + s3) / 3;
-    //  if(0 <= score < 60) {return	'F'}
-    // else if(60 <= score < 70) {return	'D'}
-    // else if (70 <= score < 80) {return	'C'}
-    // else if(80 <= score < 90) {return	'B'}
-    // else if(90 <= score <= 100) {return	'A'}
+      score = (s1 + s2 + s3) / 3;
+     if( score < 60) {return	'F'}
+    else if(score < 70) {return	'D'}
+    else if ( score < 80) {return	'C'}
+    else if(score < 90) {return	'B'}
+    else if(score <= 100) {return	'A'}
 
  
 
