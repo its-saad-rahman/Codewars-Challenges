641e0e7d9f9498117d2817b00438dd8f32cb4124
Saad Rahman <itssaadrahman@gmail.com>
2021-02-28 13:46:11 +0600

    commit 11

--- js/challenge-011.js
+++ js/challenge-011.js
@@ -11,3 +11,8 @@ Note: the input will always be valid.
 For example:
 
 5, 10, 2  -->  17 */
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    // code goes here
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  }
