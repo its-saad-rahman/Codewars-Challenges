4102fb73c56becff451e1405999db204b2b616b7
Saad Rahman <itssaadrahman@gmail.com>
2021-02-28 19:06:24 +0600

    challenge 17

--- js/challenge-017.js
+++ js/challenge-017.js
@@ -1,1 +1,13 @@
- 
+/*Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+doubleChar("String") ==> "SSttrriinngg"
+
+doubleChar("Hello World") ==> "HHeelllloo  WWoorrlldd"
+
+doubleChar("1234!_ ") ==> "11223344!!__  "
+Good Luck! */ 
+
+function doubleChar(str) {
+    // Your code here
+   return str.split('').map(a => a.repeat(2)).join('');
+  }
