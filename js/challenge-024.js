2489f0a13fd58202c2a7f6b83fea2e9113ed9246
Saad Rahman <itssaadrahman@gmail.com>
2021-03-01 21:55:02 +0600

    challenge 29

--- js/challenge-024.js
+++ js/challenge-024.js
@@ -1,1 +1,11 @@
- 
+ /**Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+Example:
+
+myArr = ['Keep', 'Remove', 'Keep', 'Remove', 'Keep', ...];
+None of the arrays will be empty, so you don't have to worry about that */
+
+function removeEveryOther(arr){
+    //your code here
+   return arr.filter((a,i) => i % 2 === 0);
+  }
