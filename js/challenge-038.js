08d6a7eea8a18b4b940e652bd1027db15001a450
Saad Rahman <itssaadrahman@gmail.com>
2021-03-02 22:44:18 +0600

    challenge 40

--- js/challenge-038.js
+++ js/challenge-038.js
@@ -1,1 +1,16 @@
- 
+ /**Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter. */
+const quarterOf = (m) => {
+    // Your code here
+    if(m <= 3){
+      return 1;
+    } else if(m <= 6){
+      return 2;
+    } else if( m <= 9){
+      return 3;
+    } else {
+      return 4
+    }
+    
+  }
--- js/challenge-040.js
+++ js/challenge-040.js
@@ -1,1 +1,15 @@
- 
+ /**Simple challenge - eliminate all bugs from the supplied code so that the code runs and outputs the expected value. Output should be the length of the longest word, as a number.
+
+There will only be one 'longest' word. */
+function findLongest(str) {
+
+    let longest = 0
+    const spl = str.split(" ").filter(a => {
+        if(a.length > longest ){
+            longest = a.length;
+        }
+    });
+    return longest;
+    
+
+}
