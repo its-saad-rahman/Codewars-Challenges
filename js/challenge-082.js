2b9dd74f2f5357bcd1124a0a75c2c5a9eba30418
Saad Rahman <itssaadrahman@gmail.com>
2021-03-07 19:50:52 +0600

    challenge 85

--- js/challenge-082.js
+++ js/challenge-082.js
@@ -1,1 +1,5 @@
- 
+ /**Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0. */
+function combat(health, damage) {
+    const currentHealth = health - damage;
+   return currentHealth > 0 ? currentHealth : 0;
+  }
--- js/challenge-084.js
+++ js/challenge-084.js
@@ -1,1 +1,18 @@
- 
+/**#To square(root) or not to square(root)
+
+Write a method, that will get an integer array as parameter and will process every number from this array.
+Return a new array with processing every number of the input-array like this:
+
+If the number has an integer square root, take this, otherwise square the number.
+
+[4,3,9,7,2,1] -> [2,9,3,49,4,1]
+The input array will always contain only positive numbers and will never be empty or null. */ 
+
+
+function squareOrSquareRoot(array) {
+  const  isSquare = function (n) {
+        return Math.sqrt(n) % 1 === 0;
+    };
+    return array.map(number => isSquare(number) ? Math.sqrt(number) : number ** 2)
+}
+
